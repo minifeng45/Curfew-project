ad33b115a7b59b0a4973a19acfbe1ef9e01534a2
何宇峰 <supermonk00@heyufengs-MacBook-Pro.local>
2020-06-01 20:20:35 -0400

    2020/06/01.update

---
+++ Covid-19 Bar chart race in Carib/countryname.picture.sort.R
@@ -0,0 +1,38 @@
+library(rvest)
+library(git2r)
+library(dplyr)
+library(stringr)
+
+# sort interested country list from wiki
+countryname = read_html("https://en.wikipedia.org/wiki/List_of_Caribbean_countries_by_population") %>% 
+  html_nodes("table") %>% 
+  { .[[1]] } %>% 
+  html_table() %>% 
+  #generate new column, based on Country(or dependent territory). Remove parentheses.
+  dplyr::mutate(Country = sub("\\(.*\\)", "", gsub("\\[.*\\]", "", `Country(or dependent territory)`))) %>%
+  # trim the undifined countries in the list ###
+  dplyr::filter(Country != "Saint Martin ",Country!="Total")%>%
+  dplyr::select(Country) %>%
+  distinct() 
+
+# tranform data.frame to character, remove space
+countryname  = gsub("^\\s|\\s$", "", countryname$Country) 
+
+
+
+
+
+# obtain ISO Alpha-2 Numeric Country Codes 
+# package citation: https://cran.r-project.org/web/packages/countrycode/countrycode.pdf
+library(countrycode)
+code = countrycode(c(countryname),
+            origin = 'country.name', destination = 'iso2c') %>%
+  toupper() #Captial letter to small letter
+
+
+# obtain the url address by replacing the country code
+flagaddress = c("https://www.countryflags.io/countrycode/flat/64.png") %>% 
+  str_replace(., "countrycode", code)
+
+
+         
--- Covid-19 Bar chart race in Carib/covid in St..R
+++ Covid-19 Bar chart race in Carib/covid in St..R
@@ -1,86 +1,57 @@
 ####Carib confirmed cases of COVID-19
-library(readr)
+library(utils)
 library(dplyr)
 library(lubridate)
 library(devtools)
+library(readr)
 
 # obtain the target countries list, flag information and code
-devtools::source_url("https://raw.githubusercontent.com/supermonk00/Curfew-project/master/Covid-19%20Bar%20chart%20race%20in%20Carib/countryname%26picturesort.R")
+source("/Users/supermonk00/Desktop/programing/R/Curfew-project/Covid-19 Bar chart race in Carib/countryname.picture.sort.R")
+
 #insert data from European Centre for Disease Prevention and Control(ECDC)
 #https://www.ecdc.europa.eu/en
+rawdata = read.csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", 
+                   na.strings = "", fileEncoding = "UTF-8-BOM")
 
-
-ECDCdataclean = function(
-  ECDCdata = "csv.dms",
-  countrycode = Capcode,
-  country_flag= flagaddress,
-  filename= "filename.csv",
-  form ="culumative"           #individual or cumulative
-){
-  #insert data
+ECDC.mining = function(savename){
   
-  rawdata <- read_csv(ECDCdata)
-  #sort the specific, interested countries
+  # select the caribbean country
+  carib= rawdata %>% 
+    dplyr::filter(geoId %in% code)
   
-  source_case = function(geoID){
-    carib = rawdata %>% 
-      dplyr::filter(geoId == geoID)
-    return(carib)
-  }
-  countrycode_matrix = matrix(c(countrycode),nrow=1)
-  #layout the raw data: (dateRep, day, month, cases, deaths, countriesAndTerritories, geoId, countryterritory)
+  # arrange the order by month/day
+  carib.by.timeseries = carib %>%
+    group_by(month,day)%>%
+    arrange(month,day) %>%
+    filter(cases != 0)
   
-  infectionbycountry_Rawdata = lapply(countrycode_matrix,source_case)
-  
-  ###bind into an dataframe
-  #wholedata set in one frame
-  
-  whole_data = c()
-  for ( i in (1:length(infectionbycountry_Rawdata))) {  #Note:length(country_Rawdata) = how many countries
-    whole_data = rbind(whole_data,infectionbycountry_Rawdata[[i]]) #automatically erased the undefined countries
-  }
-  # chose the virus settled date
-  # check the fist case in Caribean area
-  orderday = data.frame()
-  for (i in min(whole_data$month):max(whole_data$month)) {
-    for (j in 1:31) {
-      a = whole_data[which(whole_data$month == i & 
-                             whole_data$day == j & 
-                             whole_data$cases != 0),]
-      if (nrow(a) != 0){
-        orderday = rbind(orderday,a)
-      }
-    }
-  }
-
-
   # required package: lubridate::dmy
-  day = seq(dmy(orderday$dateRep[1]),
-            dmy(orderday$dateRep[nrow(orderday)]), "days") %>% 
-    format(., "%d/%m/%Y")
+  day = seq(dmy(carib.by.timeseries$dateRep[1]),
+            dmy(carib.by.timeseries$dateRep[nrow(carib.by.timeseries)]), "days") %>% 
+    format(., "%d/%m/%Y") 
   
   # establish the platform for filling the data
-  platform =  matrix(0, ncol = length(day), nrow = length(countryname))
-  dataset = data.frame(countryname, countrycode, country_flag, platform)
-  colnames(dataset) = c("Country","geoID","Flag",day)
+  case.fill.platform =  matrix(0, 
+                               ncol = length(day), 
+                               nrow = length(countryname)
+  )
+  Dataset = data.frame(countryname, code, flagaddress, case.fill.platform )
+  colnames(Dataset) = c("Country","geoID","Flag",day)
   
   # every cases, fill in to their belonging country x day
   
-    for (i in 1:nrow(orderday)){
-      dataset[dataset$geoID == orderday$geoId[i],
-              which(colnames(dataset) == orderday[i,]$dateRep)] = orderday$cases[i]
-    }
-  if (form == "culumative") {
-    for (j in 5:(length(day)+3)) {
-      dataset[,j] = dataset[,j]+ dataset[,j-1]
-    }
-    write.csv(dataset, filename, row.names = FALSE)
+  for (i in 1:nrow(carib.by.timeseries)){
+    Dataset[Dataset$geoID == as.character(carib.by.timeseries$geoId[i]),
+            which(colnames(Dataset) == carib.by.timeseries[i,]$dateRep)] = carib.by.timeseries$cases[i]
   }
+  # culumative: today's cases = yesterday's + today's
+  for (j in 5:(ncol(Dataset))) {
+    Dataset[,j] = Dataset[,j]+ Dataset[,j-1]
+  }
+  write.csv(Dataset, savename, row.names = FALSE,)
 }
-
   
-
-
+ECDC.mining(savename = "covid19.2020.06.01")
 
 
 
