2afd07fb791dd7d8cd9d2bda07603b68513481a1
supermonk00 <supermonk00@gmail.com>
2020-08-13 16:35:42 +0800

    2020/08/13 a new FUN

---
+++ high quality png plot output/high.quality.png.output.R
@@ -0,0 +1,18 @@
+###FUNCTION: high quality png.file output
+png.output = function(plot = p,
+                      filename = "",
+                      width = 1600,
+                      height = 1600,
+                      res = 500,
+                      family = "GB1"){
+  png(
+    filename = filename, # must .png!!!
+    type = "cairo", # avoid zigzag
+    res = res, # resolution
+    width = width, height = height,
+    bg = "transparent", # transparent background 
+    family=family #Font change
+  )
+  plot
+  dev.off()
+}
